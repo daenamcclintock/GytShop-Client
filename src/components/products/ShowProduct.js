037a26963962b2c1b15e7b4057c7ae238786b549
yazeedhani <haniyazeed9@gmail.com>
2022-04-03 12:26:25 -0600

    Can now create a new product.

--- src/components/products/ShowProduct.js
+++ src/components/products/ShowProduct.js
@@ -1,11 +1,17 @@
 import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { getOneProduct } from "../../api/products";
-import { Spinner, Container, Card, Button } from "react-bootstrap";
+import { Spinner, Container, Card, Button, Form } from "react-bootstrap";
 
-const ShowProduct = () => {
+const ShowProduct = (props) => {
     const [product, setProduct] = useState(null)
     const {productId} = useParams()
+    const { user, msgAlert } = props
+    const navigate = useNavigate()
+
+    const formControlStyle = {
+        width: '4%'
+    }
 
     console.log('product ID', productId)
 
@@ -15,7 +21,43 @@ const ShowProduct = () => {
             .catch(console.error)
     }, [productId])
 
-    console.log('product: ', product)
+    // console.log('product: ', product)
+
+    const handleChange = (e) => {
+        e.persist()
+
+        setProduct(prevProduct => {
+            const name = e.target.name
+            let value = e.target.value
+            
+            const updatedValue = { [name]:value }
+
+            return {...prevProduct, ...updatedValue}
+        })
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+
+        // createProduct(user, product)
+        //     .then(res => {navigate(`/products/${res.data.product._id}`)})
+        //     // .then(res => console.log('product id: ', res.data.product.))
+        //     // Then we send success message
+        //     .then( () =>
+        //         msgAlert({
+        //             heading: 'Product Added to cart! Success!',
+        //             message: 'Product added to cart',
+        //             variant: 'success',
+        //     }))
+        //     // if there is an error, we'll send an error message
+        //     .catch( () =>
+        //         msgAlert({
+        //             heading: 'Oh No!',
+        //             message: 'Product could not be added to cart',
+        //             variant: 'danger',
+        //     }))
+        console.log('submitted!')
+    }
 
     if(!product)
     {
@@ -28,13 +70,26 @@ const ShowProduct = () => {
         )
     }
 
+    // When you click 'Add To Cart' you need to send the productId to an order route to push it to productsOrdered array
+
     return(
         <>
             <Container>
                 <h3>{product.name}</h3>
                 <p>${product.price}</p>
+                <p>In-stock: {product.stock}</p>
                 <p>{product.description}</p>
-                <Button className="m-2" variant="primary">Add To Cart</Button>
+                <Form onSubmit={handleSubmit}>
+                    <Form.Label>Qty:</Form.Label>
+                    <Form.Control 
+                        onChange={handleChange} 
+                        type='number'   
+                        value={product.stock}
+                        name='stock'
+                        style={formControlStyle}
+                    />
+                    <Button className="m-2" variant="primary" type='submit'>Add To Cart</Button>
+                </Form>
             </Container>
         </>
     )
--- src/components/shared/ProductForm.js
+++ src/components/shared/ProductForm.js
@@ -15,40 +15,36 @@ const ProductForm = (props) => {
                     name='name'
                     onChange={handleChange}
                 />
-
-            <Form.Label>Description of Product</Form.Label>
-                <Form.Control as='textarea'
-                    placeholder="Product description"
-                    value={product.description}
-                    name='description'
-                    onChange={handleChange}
-                />
-
-            <Form.Label>Category of Product</Form.Label>
-                <Form.Control
-                    placeholder="Category"
-                    value={product.category}
-                    name='category'
-                    onChange={handleChange}
-                />
-
-            <Form.Label>Price of Product</Form.Label>
-                <Form.Control
-                    type='number'
-                    placeholder="Price of Product"
-                    value={product.price}
-                    name='price'
-                    onChange={handleChange}
-                />
-
-            <Form.Label>Current Stock</Form.Label>
-                <Form.Control
-                    type='number'   
-                    placeholder="Number in Stock"
-                    value={product.stock}
-                    name='stock'
-                    onChange={handleChange}
-                />
+                <Form.Label>Description of Product</Form.Label>
+                    <Form.Control as='textarea'
+                        placeholder="Product description"
+                        value={product.description}
+                        name='description'
+                        onChange={handleChange}
+                    />
+                <Form.Label>Category of Product</Form.Label>
+                    <Form.Control
+                        placeholder="Category"
+                        value={product.category}
+                        name='category'
+                        onChange={handleChange}
+                    />
+                <Form.Label>Price of Product</Form.Label>
+                    <Form.Control
+                        type='number'
+                        placeholder="Price of Product"
+                        value={product.price}
+                        name='price'
+                        onChange={handleChange}
+                    />
+                <Form.Label>Current Stock</Form.Label>
+                    <Form.Control
+                        type='number'   
+                        placeholder="Number in Stock"
+                        value={product.stock}
+                        name='stock'
+                        onChange={handleChange}
+                    />
                 <Button type='submit'>Submit</Button>
             </Form>
         </Container>
