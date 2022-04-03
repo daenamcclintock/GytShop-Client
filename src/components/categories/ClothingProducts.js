e909cfadfbf6e1c602030929da2631af567bf1c9
yazeedhani <haniyazeed9@gmail.com>
2022-04-03 14:52:51 -0600

    Category dropdown list in INDEX page is working.

--- src/components/categories/ClothingProducts.js
+++ src/components/categories/ClothingProducts.js
@@ -15,7 +15,7 @@ const ClothingProducts = (props) => {
     useEffect(() => {
         getAllClothing()
             .then(res => {
-                setProducts(res.data.products)
+                setProducts(res.data.clothing)
             })
             .catch(console.error)
     }, [])
@@ -29,28 +29,26 @@ const ClothingProducts = (props) => {
 
     let productCards
 
-    if (products.length > 0) {
-        productCards = products.map(product => {
-            if (product.category === 'clothing') {
-                return (
-                    <Card key={product._id} style={{ width: '30%' }} className="m-2">
-                        <Card.Img variant="top" src="" />
-                        <Card.Title className='m-2'>{product.name}</Card.Title>
-                        <Card.Body>
-                            <Card.Text>Seller: {product.owner.username}</Card.Text>
-                            <Card.Text>
-                                <Link to={`/products/${product._id}`}>View</Link>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                )
-            }
-        })
+    if(products.length > 0) {
+        productCards = products.map(product => 
+            (
+                <Card key={product._id} style={{ width: '30%' }} className="m-2">
+                    <Card.Img variant="top" src="" />
+                    <Card.Title className='m-2'>{product.name}</Card.Title>
+                    <Card.Body>
+                        <Card.Text>Seller: {product.owner.username}</Card.Text>
+                        <Card.Text>
+                            <Link to={`/products/${product._id}`}>View</Link>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            )
+        )
     }
 
     return (
         <>
-            <h3>Browse Electronics Products</h3>
+            <h3>Browse Clothing Products</h3>
             <div style={cardContainerLayout}>
                 {productCards}
             </div>
--- src/components/categories/CollectiblesProducts.js
+++ src/components/categories/CollectiblesProducts.js
@@ -15,7 +15,7 @@ const CollectibleProducts = (props) => {
     useEffect(() => {
         getAllCollectibles()
             .then(res => {
-                setProducts(res.data.products)
+                setProducts(res.data.collectibles)
             })
             .catch(console.error)
     }, [])
@@ -29,23 +29,21 @@ const CollectibleProducts = (props) => {
 
     let productCards
 
-    if (products.length > 0) {
-        productCards = products.map(product => {
-            if (product.category === 'collectibles') {
-                return (
-                    <Card key={product._id} style={{ width: '30%' }} className="m-2">
-                        <Card.Img variant="top" src="" />
-                        <Card.Title className='m-2'>{product.name}</Card.Title>
-                        <Card.Body>
-                            <Card.Text>Seller: {product.owner.username}</Card.Text>
-                            <Card.Text>
-                                <Link to={`/products/${product._id}`}>View</Link>
-                            </Card.Text>
-                        </Card.Body>
-                    </Card>
-                )
-            }
-        })
+    if(products.length > 0) {
+        productCards = products.map(product =>
+            (
+                <Card key={product._id} style={{ width: '30%' }} className="m-2">
+                    <Card.Img variant="top" src="" />
+                    <Card.Title className='m-2'>{product.name}</Card.Title>
+                    <Card.Body>
+                        <Card.Text>Seller: {product.owner.username}</Card.Text>
+                        <Card.Text>
+                            <Link to={`/products/${product._id}`}>View</Link>
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            )
+        )
     }
 
     return (
